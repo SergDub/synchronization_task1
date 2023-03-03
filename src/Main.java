8db96f12fea5050cdd61d0ccdadccaa9a54e6981
Сергей Дубовский <svdub@mail.ru>
2023-03-03 15:55:32 +0300

    Задача 2. Частота операций

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     final static String LETTERS = "RLRFR";
@@ -8,43 +6,72 @@ public class Main {
     final static int AMOUNT_OF_THREADS = 1000;
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
-    public static void main(String[] args) {
-        for (int i = 0; i < AMOUNT_OF_THREADS; i++) {
-            new Thread(() -> {
-                String route = generateRoute(LETTERS, ROUTE_LENGTH);
-                int freguency = (int) route.chars().filter(ch -> ch == 'R').count();
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threadList = new ArrayList<>();
 
+        Thread printer = new Thread(() -> {
+            while (!Thread.interrupted()) {
                 synchronized (sizeToFreq) {
-                    if (sizeToFreq.containsKey(freguency)) {
-                        sizeToFreq.put(freguency, sizeToFreq.get(freguency) + 1);
-
-                    } else {
-                        sizeToFreq.put(freguency, 1);
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        return;
                     }
+                    printLeader();
                 }
-            }).start();
+            }
+        });
+        printer.start();
+
+        for (int i = 0; i < AMOUNT_OF_THREADS; i++) {
+            threadList.add(getThread());
         }
+
+        for (Thread thread : threadList) {
+            thread.start();
+            Thread.sleep(1);
+        }
+
+        for (Thread thread : threadList) {
+            thread.join();
+        }
+
+        printer.interrupt();
+    }
+
+    public static String generateRoute(String letters, int length) {
+        Random random = new Random();
+        StringBuilder route = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            route.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return route.toString();
+    }
+
+    public static void printLeader() {
         Map.Entry<Integer, Integer> max = sizeToFreq
                 .entrySet()
                 .stream()
                 .max(Map.Entry.comparingByValue())
                 .get();
-        System.out.println("Самое частое количество повторений " + max.getKey() + " (встретилось " + max.getValue() + " раз)");
+        System.out.println("Текущий лидер " + max.getKey() + " (встретилось " + max.getValue() + " раз)");
 
-        System.out.println("Другие размеры: ");
-        sizeToFreq
-                .entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue())
-                .forEach (e -> System.out.println(" - " + e.getKey() + " (" + e.getValue() + " раз)"));
     }
 
-    public static String generateRoute (String letters, int length) {
-        Random random = new Random();
-        StringBuilder route  = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            route.append(letters.charAt(random.nextInt(letters.length())));
-        }
-        return route.toString();
+    public static Thread getThread() {
+        return new Thread(() -> {
+            String route = generateRoute(LETTERS, ROUTE_LENGTH);
+            int frequency = (int) route.chars().filter(ch -> ch == 'R').count();
+
+            synchronized (sizeToFreq) {
+                if (sizeToFreq.containsKey(frequency)) {
+                    sizeToFreq.put(frequency, sizeToFreq.get(frequency) + 1);
+
+                } else {
+                    sizeToFreq.put(frequency, 1);
+                }
+                sizeToFreq.notify();
+            }
+        });
     }
 }
